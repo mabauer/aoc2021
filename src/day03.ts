416eca60e4b6d8c5b9494f19bb07e0645471060d
Markus Bauer <mkbauer@web.de>
2021-12-04 16:33:38 +0100

    Solution for day 3, part 2.

--- src/day03.ts
+++ src/day03.ts
@@ -9,6 +9,7 @@ function countChars(s: string, ch: string) : number {
 }
 
 function part1(lines : string[]) {
+    lines = lines.filter(l => l.length > 0);
     let gamma : string = "";
     let epsilon : string = ""; 
     const len = lines[0].length;
@@ -17,7 +18,7 @@ function part1(lines : string[]) {
         for (let line of lines) {
             col += line[i];
         }
-        console.log(col);
+        // console.log(col);
         const zeros = countChars(col, "0");
         const ones = countChars(col, "1");
         if (ones > zeros) {
@@ -36,9 +37,66 @@ function part1(lines : string[]) {
     return result;
 }
 
+enum STRATEGY {
+    MOST_COMMON,
+    LEAST_COMMON,
+}
+
+function compute_rating(lines : string[], strategy=STRATEGY.MOST_COMMON) {
+    let remaining = lines.filter(l => l.length > 0);
+    const len = remaining[0].length;
+    for (let i = 0; i < len; i++) {
+        let col = "";
+        for (let line of remaining) {
+            col += line[i];
+        }
+        // console.log(col);
+        const zeros : number[] = col.split("")
+            .map( (elem, i) => (elem === "0")? i :  -1 )
+            .filter(elem => (elem >= 0));
+        const ones : number[] = col.split("")
+            .map( (elem, i) => (elem === "1")? i :  -1 )
+            .filter(elem => (elem >= 0));
+        // console.log(`ones: ${ones.length}`);
+        // console.log(`zeros: ${zeros.length}`);
+        if (strategy == STRATEGY.MOST_COMMON) {
+            if (ones.length >= zeros.length) {
+                // remove zeros from remaining
+                for (let zero of zeros.reverse()) {
+                    remaining.splice(zero, 1);
+                }
+            }
+            else {
+                // remove ones from remaining
+                for (let one of ones.reverse()) {
+                    remaining.splice(one, 1);
+                }
+            }
+        } else {
+            if (ones.length >= zeros.length) {
+                // remove ones from remaining
+                for (let one of ones.reverse()) {
+                    remaining.splice(one, 1);
+                }
+            } else {
+                // remove zeros from the remaining
+                for (let zero of zeros.reverse()) {
+                    remaining.splice(zero, 1);
+                }               
+            }
+        }
+        // console.log(remaining);
+        if (remaining.length == 1)
+            break;
+    }
+    const rate = parseInt(remaining[0], 2);
+    return rate;
+}
+
 function part2(lines : string[]) {
-    let result = 0;
-    return result;
+    const o2_generator_rate = compute_rating(lines, STRATEGY.MOST_COMMON);
+    const co2_scrubber_rate = compute_rating(lines, STRATEGY.LEAST_COMMON);
+    return o2_generator_rate * co2_scrubber_rate;
 }
 
 export { countChars, part1, part2 };
