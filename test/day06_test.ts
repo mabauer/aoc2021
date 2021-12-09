9f4babced2cd8c7ef958ec069d342bae2f51d2b8
Markus Bauer <mkbauer@web.de>
2021-12-09 13:18:08 +0100

    Day 6: Fix test cases

--- test/day06_test.ts
+++ test/day06_test.ts
@@ -5,18 +5,13 @@ import { part1, part2, Swarm } from '../src/day06';
 
 suite("Day06", () => {
   
-    test("example for part 1", () => {
+    test("example for part 1 and 2", () => {
         let example = [3,4,3,1,2];
         const swarm = new Swarm(example);
         swarm.play(18);
         assert.equal(swarm.getNumberOfFishes(), 26);
         swarm.play(80-18);
         assert.equal(swarm.getNumberOfFishes(), 5934);
-    });
-
-    test("example for part 2", () => {
-        let example = ["1", "2", "3"];
-        assert.equal(part2(example), 0);
     });
 
 });
